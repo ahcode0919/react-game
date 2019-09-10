5640b500b1fc7f7c5695739e6b1922cc8f8cec4c
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2019-09-09 20:48:59 -0700

    Create squares with for loops

--- src/index.js
+++ src/index.js
@@ -27,23 +27,21 @@ class Board extends React.Component {
   }
 
   render() {
+    let rows = [];
+    let count = 0;
+    for(let i = 0; i < 3; i++) {
+      let squares = [];
+      for (let j = 0; j < 3; j++) {
+        squares.push(this.renderSquare(count));
+        count += 1;
+      }
+      rows[i] = <div className="board-row">
+        {squares}
+      </div>;
+    }
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {rows}
       </div>
     );
   }
