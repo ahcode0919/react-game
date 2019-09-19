06f564c81c9cd03e9a7a5756c698c8afd0cf1e82
A. Hinton <7063649+ahcode0919@users.noreply.github.com>
2019-09-19 00:21:04 -0700

    Add Navbar and background color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { Container, Jumbotron, Row, Col, Navbar, Nav } from 'react-bootstrap';
 import Game from './components/game';
 
 const App = () => (
-  <Game />
+  <Container className="bg-secondary app" fluid="true">
+    <Navbar bg="dark" fixed="top">
+      <Navbar.Brand href="#" className="text-light">Tic-Tac-Toe</Navbar.Brand>
+      <Navbar.Collapse className="justify-content-end">
+        <Nav>
+          <Nav.Link className="text-light" href="https://github.com/ahcode0919/react-game">Github</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+      <Row>
+        <Col>
+          <Game />
+        </Col>
+      </Row>
+  </Container>
 );
 
 export default App;
